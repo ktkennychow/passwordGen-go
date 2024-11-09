39d3d6ddc0a9d6e1b2d9d60b4239026288fc034c
ktkennychow <ktkennychow@gmail.com>
2024-11-09 16:34:27 +0900

    feat: implement password generation functionality
    Adds a new function to generate random passwords with customizable length
    and optional special characters. Updates the root command to use this
    functionality and adds flags for password length and special characters.

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,53 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+func generateRandomPassword(length int, sC bool) string {
+	alphanumericals := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	specialChars := "!@#$%^&*()_+-=[]{}|;:,.<>?~`'"
+	allChars := ""
+	result := make([]byte, length)
 
+	if sC {
+		allChars = alphanumericals + specialChars
+	} else {
+		allChars = alphanumericals
+	}
+
+	for i := 0; i < length; i++ {
+		result[i] = allChars[rand.Intn(len(allChars))]
+	}
+
+	return string(result)
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "passwordGen-go",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "Explain passwordGen",
+	Short: `PasswordGen is a CLI application for password generation.`,
+	Long: `PasswordGen is a CLI application for password generation.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+  --length, -t    to specify password length. (default 8)
+  --special, -s   to include special characters in the generated password`,
+
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		length, _ := cmd.Flags().GetInt("length")
+		sC, _ := cmd.Flags().GetBool("special")
+
+		generatedPassword := generateRandomPassword(length, sC)
+		fmt.Println("Generated Password: ", generatedPassword)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,7 +68,6 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("special", "s", false, "--spacial/-s flag is used to include special characters in the generated password")
+	rootCmd.Flags().IntP("length", "l", 8, "--length/-t is used to   to specify password length (default 8)")
 }
-
-
